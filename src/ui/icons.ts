ef1e8acc6798e36af56168c3bef3fea73bb6b09f
Sahil Shah <sahilshah.rr@gmail.com>
2025-11-20 03:38:34 +0530

    feature: Use correct HackerNews icon

--- src/ui/icons.ts
+++ src/ui/icons.ts
@@ -1,7 +1,7 @@
 import { addIcon } from 'obsidian';
 
 export const icons: Record<string, string> = {
-    hackernews: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 24" fill="none" fill-opacity="0.0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book-open"><path d="M21.27,0c-1.174,2.021-5.5,9.464-5.532,9.499h-0.11C15.595,9.464,11.424,2.073,10.253,0H3.148l9.253,15.034v15.927h6.157 V15.034L27.812,0H21.27z"/></svg>`,
+    hackernews: `<svg xmlns="http://www.w3.org/2000/svg" aria-label="Hacker News" role="img" viewBox="0 0 512 512"><path d="m0 0H512V512H0" fill="#f60"/><path fill="#fff" d="m124 91h51l81 162 81-164h51L276 293v136h-40V293"/></svg>`,
 };
 
 export const addIcons = (): void => {
